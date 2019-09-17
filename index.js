12e3fc1cc77f9598c79ab2fe7e2b0f9f00586a06
Donna Amos <donnacamos.developer@gmail.com>
2019-09-17 14:31:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+let Countable = (superclass) => class extends superclass{
+  static get counter(){
+    superclass._counter = (superclass._counter || 0) + 1;
+    return superclass._counter;
+  }
+
+  constructor(...args){
+    super(...args);
+    this._id = this.constructor.counter;
+  }
+
+  get id(){
+    return this._id;
+  }
+}
+
+class Neighborhood{
+  static get counter(){
+    Neighborhood._counter = (Neighborhood._counter || 0) + 1;
+    return Neighborhood._counter;
+  }
+
+  constructor(name){
+    this.name = name;
+    
+  }
+}
